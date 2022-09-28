dfdf7a2a1ea7c3286a6594f727a4d736cba4bd9f
blinovmaxim <blinov.maxim@gmail.com>
2022-09-28 16:19:32 +0300

    del secret

--- shop/settings.py
+++ shop/settings.py
@@ -178,5 +178,5 @@ INTERNAL_IPS = [
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'blinov.maxim@gmail.com'
-EMAIL_HOST_PASSWORD = 'zttiaszxigwakslt'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
