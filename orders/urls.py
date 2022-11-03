2565bd4bf6eed6ab2e0892c8ec23e204054c78b9
Dmitry Alekseev <dmitrjialekseev16@gmail.com>
2022-11-03 21:06:02 +0200

    add liqpay callback

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    re_path(r'^create/$', views.order_create, name='order_create'),
+    # re_path(r'^create/$', views.order_create, name='order_create'),
+    path('create/', views.order_create, name='order_create'),
+    path('created/', views.order_created, name='order_created'),
     path('thanks/', views.thanks_page, name='thanks_page'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,49 +1,71 @@
-from django.shortcuts import render
-from .models import OrderItem
+from django.shortcuts import render, redirect
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import CartLogic
-from .liqpay import get_liqpay_params
 from ecomm.models import Product
+from .liqpay_controller import LiqpayChekout, LiqpayCallback
 
 
 def order_create(request):
     cart = CartLogic(request)
-    total_price = int(cart.get_total_price())
-
-    liqpay_params = get_liqpay_params(total_price)
-    liqpay_data = liqpay_params[0]
-    liqpay_signature = liqpay_params[1]
 
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
+
             for item in cart:
                 OrderItem.objects.create(order=order,
                                          product=item['product'],
                                          price=item['price'],
                                          quantity=item['quantity'])
-            # очистка корзины
-            cart.clear()
-            return render(request, 'orders/order/created.html',
-                          {'order': order})
+            return redirect('/orders/created')
+
     else:
         form = OrderCreateForm
 
-    return render(request, 'orders/order/create.html',
-                  {'cart': cart, 'form': form, 'data': liqpay_data, 'signature': liqpay_signature})
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+def order_created(request):
+    cart = CartLogic(request)
+    total_price = int(cart.get_total_price())
+
+    liqpay = LiqpayChekout(total_price)
+    liqpay.set_order_id()
+    data = liqpay.create_param_checkout()[0]
+    signature = liqpay.create_param_checkout()[1]
+
+    return render(request, 'orders/order/created.html', {'data': data, 'signature': signature})
 
 
 def thanks_page(request):
     cart = request.session['cart']
 
-    # del request.session['uuid']
-    request.session['cart'] = {}
-    request.session['counter_items'] = 0
+    liqpay = LiqpayCallback()
+    res = liqpay.send_request()
+    status = liqpay.get_response_status(res)
 
-    for key, value in cart.items():
-        product = Product.objects.get(pk=key)
-        product.quantity -= value['quantity']
-        product.save()
+    if status != 'success':
+        status_error = True
+
+        return render(request, 'orders/order/thanks.html', {"status": status_error})
+
+    else:
+
+        request.session['cart'] = {}
+        request.session['counter_items'] = 0
+
+        for key, value in cart.items():
+            product = Product.objects.get(pk=key)
+            product.quantity -= value['quantity']
+            product.save()
+
+        order = Order.objects.order_by('-id')[:1].get()
+        order.paid = True
+        order.save()
 
     return render(request, 'orders/order/thanks.html')
+
+
+
